15d757fe5910db2a90d7d5a2134dea5fb2857362
Hariharan M <hariharan@gmail.com>
2026-06-17 23:08:25 +0530

    Add new features in the project

---
+++ src/main/java/com/securebank/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.securebank.controller;
+
+import com.securebank.dto.request.RegisterRequest;
+import com.securebank.dto.response.AuthResponse;
+import com.securebank.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+}
---
+++ src/main/java/com/securebank/service/AuthService.java
@@ -0,0 +1,37 @@
+package com.securebank.service;
+
+import com.securebank.dto.request.RegisterRequest;
+import com.securebank.dto.response.AuthResponse;
+import com.securebank.entity.User;
+import com.securebank.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            return new AuthResponse("Email already exists", false);
+        }
+
+        User user = new User();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setRole("USER");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setIsActive(true);
+
+        userRepository.save(user);
+        return new AuthResponse("Registration successful", true);
+    }
+}
