67c49a94404f4fa6c98e117a202e4d4fa0919847
Hariharan M <hariharan@gmail.com>
2026-06-18 18:42:45 +0530

    added login response and request files and also updated the authController

--- src/main/java/com/securebank/controller/AuthController.java
+++ src/main/java/com/securebank/controller/AuthController.java
@@ -6,10 +6,9 @@ import com.securebank.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.securebank.dto.request.LoginRequest;
+import com.securebank.dto.response.LoginResponse;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,6 +20,11 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request){
         return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
     }
 
 }
--- src/main/java/com/securebank/service/AuthService.java
+++ src/main/java/com/securebank/service/AuthService.java
@@ -1,10 +1,15 @@
 package com.securebank.service;
 
+import com.securebank.dto.request.LoginRequest;
 import com.securebank.dto.request.RegisterRequest;
 import com.securebank.dto.response.AuthResponse;
+import com.securebank.dto.response.LoginResponse;
 import com.securebank.entity.User;
 import com.securebank.repository.UserRepository;
+import com.securebank.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
@@ -15,6 +20,21 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public LoginResponse login(LoginRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(()-> new RuntimeException("User not found"));
+
+        String token = jwtUtil.generateToken(user.getEmail(),user.getRole());
+        return new LoginResponse(token, user.getEmail(),user.getRole());
+    }
+
 
     public AuthResponse register(RegisterRequest request) {
         if (userRepository.existsByEmail(request.getEmail())) {
